aee63647d27684e93c72680aca1d18949af9d6ca
William Mian <william_mian@yahoo.com.br>
2022-01-24 09:47:55 -0300

    initial

---
+++ src/br/com/wm/loja/orcamento/Orcamento.java
@@ -0,0 +1,17 @@
+package br.com.wm.loja.orcamento;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+	
+	private BigDecimal valor;
+	
+	public Orcamento(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal gerValor() {
+		return valor;
+	}
+
+}
