96307882187810063d30022466f581c084398fb2
William Mian <william_mian@yahoo.com.br>
2022-01-28 10:46:16 -0300

    aplicando o pattern Decorator

--- src/br/com/wm/loja/imposto/Imposto.java
+++ src/br/com/wm/loja/imposto/Imposto.java
@@ -4,8 +4,25 @@ import java.math.BigDecimal;
 
 import br.com.wm.loja.orcamento.Orcamento;
 
-public interface Imposto {
+//Decorator
+public abstract class Imposto {
 	
-	BigDecimal calcular(Orcamento orcamento);
+	private Imposto outro;
+	
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+		
+		if(outro != null) {
+			valorDoOutroImposto = outro.realizarCalculo(orcamento);
+		}
+		return valorImposto.add(valorDoOutroImposto);
+	}
 
 }
