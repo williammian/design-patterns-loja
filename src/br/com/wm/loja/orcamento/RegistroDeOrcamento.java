62f062f03d24ed2b54bb97fc63b040a31ec625a5
William Mian <william_mian@yahoo.com.br>
2022-01-28 10:06:21 -0300

    API de registro de orçamento

---
+++ src/br/com/wm/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,12 @@
+package br.com.wm.loja.orcamento;
+
+public class RegistroDeOrcamento {
+
+	public void registrar(Orcamento orcamento) {
+		// chamada HTTP para a API externa
+		// URL Connection (Java)
+		// Http Cliente (Apache)
+		// lib Rest
+	}
+	
+}
